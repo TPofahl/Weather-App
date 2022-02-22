fa95a41f19f80ae083afcb540d3b180e7fbf24c4
TPofahl <thomaspofahl@yahoo.com>
2022-02-21 22:43:12 -0600

    added weather svg, footer size change

--- components/contactLinks.js
+++ components/contactLinks.js
@@ -1,9 +1,9 @@
 export default function ContactLinks () {
   return (
     <div className="flex flex-row justify-center">
-      <a href="https://www.linkedin.com/in/thomas-pofahl-b76b7521b/" target="_blank"><img src="icon-linkedin.svg" width="64" height="64"/></a>
-      <a  className="px-8" href="https://github.com/TPofahl" target="_blank"><img src="icon-github.svg" width="64" height="64"/></a>
-      <a href="https://portfolio-pofahl.vercel.app/" target="_blank"><img src="icon-portfolio.svg" width="64" height="64"/></a>
+      <a href="https://www.linkedin.com/in/thomas-pofahl-b76b7521b/" target="_blank"><img src="icon-linkedin.svg" width="44" height="44"/></a>
+      <a  className="px-8" href="https://github.com/TPofahl" target="_blank"><img src="icon-github.svg" width="44" height="44"/></a>
+      <a href="https://portfolio-pofahl.vercel.app/" target="_blank"><img src="icon-portfolio.svg" width="44" height="44"/></a>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -41,17 +41,17 @@ export default function Home() {
         <title>Weather App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className="flex py-7 justify-center bg-blue-400 w-screen">
-        <div className="flex flex-col justify-center text-2xl">Weather</div>
+      <header className="flex py-7 justify-center bg-blue-300 w-screen">
+        <div className="flex flex-col justify-center text-2xl"><a><img src="weather.svg" width="250" height="250"/></a></div>
       </header>
-      <main className="flex flex-col pt-8 items-center justify-center w-full flex-1 text-center bg-blue-300">
+      <main className="flex flex-col pt-8 items-center justify-center w-full flex-1 text-center bg-blue-400">
         <div className="flex px-0">
           <input className="flex sm:px-4 lg:px-8 bg-red-50 text-center border-t-4 rounded-lg mr-3" placeholder="Enter zip code" value={zipCode} onChange={handleChange}></input>
           <DataButton handleClick={handleButton}/>
         </div>
         <DisplayWeather weatherData={weatherData} error={error}/>
       </main>
-      <footer className="w-full items-center justify-center py-10 bg-blue-400">
+      <footer className="w-full items-center justify-center py-5 bg-blue-300">
         <ContactLinks/>
       </footer>
     </div>
